6469f2f7363a693b4c8cb1c8831dbe70811880df
llucass1998 <llucass_souza@hotmail.com>
2019-07-04 18:59:27 -0300

    Initial commit

---
+++ site.php
@@ -0,0 +1,47 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+
+    <title>Aula, Bootstrap!</title>
+</head>
+  
+
+<body>
+    <div class="container">
+    
+    <?php
+      include_once 'menu.php';
+    ?>
+    <!-- Banner -->
+    <div class="row">
+        <div class="col-sm">
+          <img src="img/banner.jpg" class="img-fluid">
+        </div>
+        </div>
+
+        <div class="row">
+        <div class="col-sm">
+                <img src="img/html5.jpg">
+              </div>
+              <div class="col-sm">
+                    <img src="img/bootstrap.jpg">
+                  </div>
+                  <div class="col-sm">
+                        <img src="img/wordpress.jpg">
+                      </div>
+                    </div>
+                    </div>
+                      <!--fim banner-->
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js" integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut" crossorigin="anonymous"></script>
+    <script src="js/bootstrap.min.js"></script>
+  </body>
+</html>
